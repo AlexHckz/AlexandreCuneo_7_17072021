cde544a6c24775d7ca59a51939a1a9b10cbb1be9
Alexandre Cuneo <alexandre.cuneo@gmail.com>
2021-07-31 16:02:19 +0200

    installing vue frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+const { Sequelize } = require('sequelize');
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,4 +12,24 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+const sequelize = new Sequelize("nom_base_de_donnees", "root", "", {
+  dialect: "mysql",
+  host: "localhost"
+});
+try {
+  sequelize.authenticate();
+  console.log('Connecté à la base de données MySQL!');
+} catch (error) {
+  console.error('Impossible de se connecter, erreur suivante :', error);
+}
+
+
 module.exports = app;
