388120b9f9443ba681f9825438171fdda42f4a90
Alexandre Cuneo <alexandre.cuneo@gmail.com>
2021-08-10 23:11:45 +0200

    frontend router and nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <Header title="Le titre de mon super site"/>
-  <Main titleMain="My Title Main"/>
-  <Footer/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/signup">Signup</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Main,
-    Footer
-  }
-}
-
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header title="Le titre de mon super site"/>
+  <Main titleMain="My Title Main"/>
+  <Footer/>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Main from '../components/Main.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
